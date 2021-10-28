c2c0fef71f1450b443c3ca2af5686373856ce8ea
Shruti Grover <shruti.grover2001@gmail.com>
2021-10-28 22:09:17 +0530

    Demo commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# React-recipe-App
+
+
+A web app to fetch different recpies from around the globe
+
+How to use :-
+
+for developers
+
+1> Clone the repo
+<br>
+2> npm install
+<br>
+3> npm start
+<br>
+4> Open the app.js and enjoy
+
---
+++ src/heading.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Heading = () =>{
+    return(
+        <div className="heading">
+            <h1>
+                The Superlative Recipe App
+            </h1>
+        </div>
+    )
+}
+
+export default Heading;
